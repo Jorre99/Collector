fce89de467e55ca2f237c8b7a95d7f6dbbf13014
jorre99 <r0796448@student.thomasmore.be>
2021-06-20 00:51:49 +0200

    file upload using filepond

--- models/book.js
+++ models/book.js
@@ -1,6 +1,4 @@
 const mongoose = require('mongoose')
-const path = require('path')
-const coverImageBasePath = 'uploads/bookCovers'
 
 // add schema
 const bookSchema = new mongoose.Schema({
@@ -24,7 +22,11 @@ const bookSchema = new mongoose.Schema({
         required: true,
         default: Date.now
     },
-    coverImageName: {
+    coverImage: {
+        type: Buffer,
+        required: true
+    },
+    coverImageType:{
         type: String,
         required: true
     },
@@ -37,13 +39,13 @@ const bookSchema = new mongoose.Schema({
 
 //.virtual creates a virtual 'clone' of the schema
 
-// get the image path for the books listing page(-> books.index.ejs)
+// use the coverImage BufferData (filepond EncodePlugin) as source to display the image in HTML
 bookSchema.virtual('coverImagePath').get(function(){
-    if (this.coverImageName != null){
-        return path.join('/', coverImageBasePath, this.coverImageName)
+    if (this.coverImage != null && this.coverImageType != null){
+        
+        return `data:${this.coverImageType};charset=utf-8;base64,${this.coverImage.toString('base64')}`
 
     }
 })
 
 module.exports = mongoose.model('Book', bookSchema)
-module.exports.coverImageBasePath = coverImageBasePath
